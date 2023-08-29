2dee32124cd443e3e47aa68596783a8235140397
Sameer Faridi <s.faridi007@gmail.com>
2023-08-29 22:20:12 +0530

    initial setup from scratch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -17,4 +16,3 @@ const config: Config = {
   },
   plugins: [],
 }
-export default config
